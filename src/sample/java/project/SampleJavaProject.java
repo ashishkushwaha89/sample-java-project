232797e77695f73807e7756573bc5b5f6f67b520
ashishkushwaha89 <kushwaha.ashish89@gmail.com>
2020-02-11 23:06:21 +0530

    Update SampleJavaProject.java
    hub change 20th line

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -21,6 +21,7 @@
 //17th line added in git hubbbbb
 //18th line eclipse
 //19th line hub
+//20th hub
 package sample.java.project;
 
 import com.beust.jcommander.JCommander;
@@ -32,6 +33,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 //extra line hub
+//2nd hub
 
 /**
  * The main class of the application. It contains the main() method,
