1783e3b491314ce35ee316b4a517f57ecaef0a5e
Ashish Kushwaha <kushwaha.ashish89@gmail.com>
2020-04-02 18:36:07 +0530

    conflict changes merge and merging done

--- src/sample/java/project/package-info.java
+++ src/sample/java/project/package-info.java
@@ -1,4 +1,11 @@
+//First comment line in Git Hub
+//eclipse 2nd line comment
 /**
  * An example Ant-based Java project.
  */
 package sample.java.project;
+//2nd line added in GitHub
+
+//3rd line git
+//4th line git
+//5th hub
